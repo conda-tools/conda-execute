d9af37158ae87589cce7bd34ebab4f375b8343a5
Phil Elson <pelson.pub@gmail.com>
2015-10-17 07:20:02 +0100

    Added a test which checks the python version of a resolved environment.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,6 +7,9 @@ tmp_script=$(dirname $0)/executable_script.sh
 
 conda execute -c "# conda execute" "# env:" "# - python" "# run_with: python" "print('hello')" -v
 
+# TODO: Assert that the version of Python is sensible
+conda execute -c "# conda execute" "# env:" "# - python" "python --version" -v
+
 conda execute -v -c <<EOF
 # conda execute
 # env:
